d617e05c45aff65896b9af787769e55fb47f86b7
Ming Chow <mchow@cs.tufts.edu>
2011-09-06 00:05:53 -0400

    Added Lab 1 files

---
+++ lab1/lab01_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int winningNums[6];
+
+	for (int count = 0; count < 100; count++) {
+		cout << lotto[count] << endl;
+	}
+	
+	return 0;
+}
