e6db004d370ab2254e2614909e037492009f4103
Ming Chow <mchow@cs.tufts.edu>
2011-09-18 23:42:29 -0400

    Added files for lab 2

---
+++ lab2/Simulation.cpp
@@ -0,0 +1,18 @@
+//
+//  Simulation.cpp
+//  Lab 2
+//
+//  COMP 15: Data Structures
+//	Fall 2011
+//  Tufts University Department of Computer Science
+//	Updated by ????
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, const char * argv[])
+{
+    return 0;
+}
