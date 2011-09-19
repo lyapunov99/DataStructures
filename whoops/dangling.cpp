f4ef5e6f9781b976388da02c1a4a0697a9606af4
Ming Chow <mchow@cs.tufts.edu>
2011-09-19 11:21:08 -0400

    Added dangling pointer example

---
+++ whoops/dangling.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int* foo()
+{
+  int count;
+  count = 18;
+  cout << "Value of local variable count: " << count << endl;
+  cout << "Address of local variable count: " << &count << endl;
+  return &count;
+}
+
+int main()
+{
+  int *d;
+  d = foo();
+  cout << "Address of d: " << d << endl;
+  cout << "Value of d: " << *d << endl;
+}
