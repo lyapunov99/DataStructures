95b5b91ed80500c289b2d5631ebf4b484f731caa
Ming Chow <mchow@cs.tufts.edu>
2012-06-05 17:17:35 -0400

    Finished List ADT (except for reverse())

--- linked_lists/List.cpp
+++ linked_lists/List.cpp
@@ -13,89 +13,134 @@ using namespace std;
 
 List::List()
 {
-    cout << "Constructing list..." << endl;
-    head = NULL;
+  cout << "Constructing list..." << endl;
+  head = NULL;
 }
 
 void List::add (string str)
 {
-    Node *newNode;
-    newNode = new Node;
-    newNode->data = str;
-    newNode->next = NULL;
-    
-    if (head == NULL) {
-        head = newNode;
+  Node *item = new Node;
+  item->data = str;
+  item->next = NULL;
+  if (head == NULL) { // list is empty
+    head = item;
+  }
+  else {
+    // add to end of list
+    Node *temp = head;
+    while (temp->next != NULL) {
+      temp = temp->next;
     }
-    else {
-        Node *temp = head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-    }
+    temp->next = item;
+  }
 }
 
 void List::remove (int index)
 {
-    // To be completed
+  Node *temp = head;
+  int listSize = this->size(); // why am I calling this only once?
+
+  if (index < listSize && index >= 0) {
+    if (index == 0) { // remove first element of list
+      temp = head;
+      head = head->next;
+      delete temp;
+    }
+    else if (index == listSize - 1) { // remove last element of list
+      for (int count = 0; count < listSize - 2; count++) {
+	temp = temp->next;
+      }
+      delete temp->next;
+      temp->next = NULL;
+    }
+    else { // remove element somewhere in the middle
+      for (int count = 0; count < index - 1; count++) {
+	temp = temp->next;
+      }
+      Node *dummy = temp->next;
+      temp->next = temp->next->next;
+      delete dummy;
+    }
+  }
 }
 
 string List::get (int index)
 {
-    // To be completed
-    return "TO BE COMPLETED";
+  Node *temp = head;
+  int listSize = this->size();
+  if (index < listSize && index >= 0) {
+    for (int count = 0; count < index; count++) {
+      temp = temp->next;
+    }
+    return temp->data;
+  }
+  return "ERROR";
 }
 
 bool List::isEmpty()
 {
-    if (head == NULL) {
-        return true;
-    }
-    return false;
+  if (head == NULL) {
+    return true;
+  }
+  return false;
 }
 
 int List::size()
 {
-    int count = 0;
-    Node *temp = head;
-    while (temp != NULL) {
-        count++;
-        temp = temp->next;
-    }
-    return count;
+  int size = 0;
+  Node *temp = head;
+  while (temp != NULL) {
+    size++;
+    temp = temp->next;
+  }
+  return size;
 }
 
 void List::reverse()
 {
-    // To be completed    
+  // Job interview question...
 }
 
 bool List::contains(string str)
 {
-    return false;
+  Node *temp = head;
+  bool found = false;
+  
+  while (temp != NULL && !found) {
+    if (str.compare(temp->data) == 0) {
+      found = true;
+    }
+    else {
+      temp = temp->next;
+    }
+  }
+  return found;
 }
 
 void List::clear()
 {
-    // To be completed    
+  while (head != NULL) {
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+  }
 }
 
 void List::print()
 {
-    Node *temp = head;
-    while (temp != NULL) {
-        cout << temp->data << endl;
-        temp = temp->next;
-    }
+  Node *temp = head;
+  while (temp != NULL) {
+    cout << temp->data << endl;
+    temp = temp->next;
+  }
 }
 
 List::~List()
 {
-    cout << "Destructing list..." << endl;
-    while (head != NULL) {
-        Node *dump = head;
-        head = head->next;
-        delete dump;
-    }
+  cout << "Destructing list" << endl;
+  while (head != NULL) {
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+  }
 }
--- linked_lists/main.cpp
+++ linked_lists/main.cpp
@@ -20,6 +20,33 @@ int main(int argc, const char * argv[])
     l->add("Doo");
     l->add("Doodoo");
     l->print();
+
+    if (l->contains("Poo")) {
+      cout << "\nPoo was found" << endl;
+    }
+    else {
+      cout << "\nPoo was not found" << endl;
+    }
+
+    if (l->contains("Harry")) {
+      cout << "\nHarry was found" << endl;
+    }
+    else {
+      cout << "\nHary was not found" << endl;
+    }
+
+    cout << "\nRemoving Dick..." << endl;
+    l->remove(1);
+    l->print();
+
+    cout << "\nRemoving last element..." << endl;
+    l->remove(l->size() - 1);
+    l->print();
+
+    cout << "\nThe second element in the list is " << l->get(1) << endl;
+
+    cout << "\nThe 99th element in the list is " << l->get(99) << endl;
+
     delete l;
     return 0;
 }
