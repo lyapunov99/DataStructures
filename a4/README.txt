2e5d55c33c56cfe428e63d9b08a4b4fa0bc2f060
Ming Chow <mchow@cs.tufts.edu>
2012-07-17 23:01:18 -0400

    Added a4 files

---
+++ a4/README.txt
@@ -0,0 +1,1 @@
+See assignment specification at http://www.cs.tufts.edu/comp/15/assignments/a4.php
---
+++ a4/a4.h
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+// Total directions one can try from each room
+#define DIRS  6
+
+// Directions in which we can try to go. 
+#define NORTH 0
+#define EAST  1
+#define SOUTH 2
+#define WEST  3
+#define UP    4
+#define DOWN  5
+
+// Goodies in each room
+#define EXIT 1
+#define GOLD 2
+
+class Room
+{
+ public:
+  int contents;      // contents of this room: from above.
+  int number;        // this room's number: between 0 and rooms-1.
+  int marker;        // marker for you to use: initially 0.
+  Room *dirs[DIRS];  // where other rooms are.
+  Room(int n = 0)
+    {
+      number = n;
+      contents = 0;
+      marker = 0;
+      for (int i = 0; i < DIRS; i++) {
+	dirs[i] = NULL;
+      }
+    }
+
+  int gold()
+  {
+    return (contents & GOLD) != 0;
+  }
+  
+  int exit() 
+  {
+    return (contents & EXIT) != 0;
+  }
+
+  void print()
+  {
+    cout << "Room " << number << " is at address " << this <<" and has"; 
+    if (gold()) {
+      cout << " gold";
+    }
+    if (exit()) {
+      cout << " exit";
+    }
+    cout << "(marker " << marker << ")" << endl;
+  }
+}; 
+
+// Description: Your routine is passed a starting position and a number of rooms.
+// Precondition: start and rooms are defined
+void quest(Room *start, int rooms); 
+
+// Description: You call this routine to actually move between rooms.
+// Precondition: dir is defined
+// Postcondition: returns 1 if you can move to the room, 0 if not
+int go(int dir);
