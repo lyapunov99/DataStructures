17e7504716df2a5a058c0571b77045ccad5f5fd8
Ming Chow <mchow@cs.tufts.edu>
2012-08-07 14:16:52 -0400

    Removed gettimeofday()

--- lab-heaping/sort.cpp
+++ lab-heaping/sort.cpp
@@ -7,7 +7,7 @@ int main()
 {
     int inputs[10] = {3, 13, 18, 12, 4, 9, 3, 2, 20, 5};
     Heap <int> h;
-    gettimeofday(&start, NULL);
+
     for (int i = 0; i < 10; i++) {
         h.enqueue(inputs[i]);
     }
