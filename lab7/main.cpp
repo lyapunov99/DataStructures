105fbe2b9fea865ee01a998b8faba46391eb8eca
Ming Chow <mchow@cs.tufts.edu>
2012-11-07 15:30:34 -0500

    Added lab7 files

---
+++ lab7/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  lab-assoc
+//
+//  COMP 15: Data Structures
+//  Tufts University Department of Computer Science
+//
+
+#include <iostream>
+#include "Assoc.h"
+
+using namespace std;
+
+int main (int argc, const char * argv[])
+{
+    Assoc presidents;
+    presidents["Monaco"] = "Anthony";
+    presidents["Clinton"] = "Bill";
+    presidents["Bush"] = "Dubya";
+    presidents["Bacow"] = "Larry";
+    cout << presidents["Clinton"] << endl;
+    presidents["Clinton"] = "Sax player";
+    cout << presidents["Bacow"] << endl;
+    cout << presidents["Bush"] << endl;
+    cout << presidents["Monaco"] << endl;
+    cout << presidents["Clinton"] << endl;
+    return 0;
+}
+
